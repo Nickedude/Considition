382f547342c38b477642bd2490580cdaf8ce8c7a
Gustav <gustav.rodstrom@gmail.com>
2017-11-07 00:11:34 +0100

    Changed the timetable list to treeset, implemented getWait and addDepartures

--- src/Graph.java
+++ src/Graph.java
@@ -1,14 +1,12 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.SortedSet;
+import java.util.*;
 
 class Graph<E> {
     HashMap<E,List<Vertex<E>>> nodes;
-    HashMap<E, SortedSet<Integer>> timetable;
+    HashMap<Vertex<E>, TreeSet<Integer>> timetable;
 
     Graph () {
         nodes = new HashMap<>();
+        timetable = new HashMap<>();
     }
 
     void addNode (E n) {
@@ -23,16 +21,24 @@ class Graph<E> {
         }
     }
 
-    boolean contains (E node) {
-        return nodes.containsKey(node);
+    void addDepartures(Vertex<E> v, Collection<Integer> times) {
+        timetable.putIfAbsent(v, new TreeSet<>());
+        timetable.get(v).addAll(times);
     }
 
     int getWait(Vertex<E> v,  int time) {
+        if(timetable.containsKey(v)) {
+            return timetable.get(v).ceiling(time);
+        }
         return 0;
     }
 
     int getDistance(E from, E to) {
         return 0;
+    }
+
+    boolean contains (E node) {
+        return nodes.containsKey(node);
     }
 
 }
